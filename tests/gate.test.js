7e78facd070f7aba3f526566cb0bbe99215dca29
Eugene Shaposhnikov <eawapownikov@gmail.com>
2022-07-06 17:37:26 +0300

    Gate. Получение ответа при проксировании.

--- tests/gate.test.js
+++ tests/gate.test.js
@@ -1,9 +1,29 @@
 const metatests = require("metatests");
-const { Gate } = require("../lib");
+const { Gate, Client } = require("../lib");
 
-metatests.testSync("Gate: proxy command", (test) => {
-    const gate = new Gate().proxyCommands('game', ['load']);
+class TransportEcho {
+    static service() {
+        return {
+            name: 'transport',
+        }
+    }
+
+    async command(code, params, options) {
+        return params;
+    }
+}
+
+metatests.testAsync("Gate: proxy command", async (test) => {
+    const gate = new Gate()
+        .addService(TransportEcho)
+        .addService(Client)
+        .proxyCommands('game', ['load']);
+
     const commands = gate.getCommands().filter(c => c.code.indexOf('game') === 0);
     test.strictEqual(commands.length, 1);
     test.strictEqual(commands.map(c => c.getCode()).sort(), ['game/load']);
+    
+    await gate.start();
+    const result = await gate.handleCommand('game/load', { any: 'param' });
+    test.strictEqual(result.toJSON(), { result: { any: 'param' }, code: 200, status: 'Success' })
 });
